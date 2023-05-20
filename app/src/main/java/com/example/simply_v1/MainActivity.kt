7a487d53811c944be25fedf80c112f943aee6f03
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-20 16:21:15 +0300

    Fundamentals

--- app/src/main/java/com/example/simply_v1/MainActivity.kt
+++ app/src/main/java/com/example/simply_v1/MainActivity.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
     }
 }
