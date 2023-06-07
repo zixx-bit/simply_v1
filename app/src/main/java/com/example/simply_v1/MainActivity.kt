d2b9ad69ae68b3a7f9810b32e4f5f655c1e6ca3b
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-06-08 00:05:35 +0300

    Constraint layout - change linear layout to constraint layout

--- app/src/main/java/com/example/simply_v1/MainActivity.kt
+++ app/src/main/java/com/example/simply_v1/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.simply_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnApply = findViewById<Button>(R.id.btnApply)
+            btnApply
     }
     override fun onStart() {
         super.onStart()
