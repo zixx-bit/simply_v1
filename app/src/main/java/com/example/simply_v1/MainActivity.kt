c61946a68c12cba679fdf956e6c976b2474cafc5
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-06-08 11:18:38 +0300

    Constraint layout - button on click listener

--- app/src/main/java/com/example/simply_v1/MainActivity.kt
+++ app/src/main/java/com/example/simply_v1/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.simply_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,10 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnApply = findViewById<Button>(R.id.btnApply)
-            btnApply
+        val firstName = findViewById<EditText>(R.id.edtFirstName)
+        val lastName = findViewById<EditText>(R.id.edtLastName)
+        val birthDate = findViewById<EditText>(R.id.edtBirtDate)
+        val country = findViewById<EditText>(R.id.edtCountry)
+
+        btnApply.setOnClickListener {
+            firstName.text.toString()
+            lastName.text.toString()
+            country.text.toString()
+            birthDate.text.toString()
+            Log.d("MainActivity", "$firstName $lastName born on $birthDate from $country")
+
+        }
     }
     override fun onStart() {
         super.onStart()
 
     }
+
 }
