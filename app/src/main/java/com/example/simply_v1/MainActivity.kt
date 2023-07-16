7f84971c4b178bfe10fec74a567bb7204749f107
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-07-16 23:37:02 +0300

    Constraint layout - Toasts and contexts

--- app/src/main/java/com/example/simply_v1/MainActivity.kt
+++ app/src/main/java/com/example/simply_v1/MainActivity.kt
@@ -12,29 +12,24 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnOrder = findViewById<Button>(R.id.btnOrder)
+        val btnToat = findViewById<Button>(R.id.btnToast)
 
-        btnOrder.setOnClickListener {
-            val rgMeat = findViewById<RadioGroup>(R.id.rgMeat)
-            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
-            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
-            val cheese = findViewById<CheckBox>(R.id.cbCheese).isChecked
-            val onions = findViewById<CheckBox>(R.id.cbOnions).isChecked
-            val salad = findViewById<CheckBox>(R.id.cbSalad).isChecked
-            val orderString = "You ordered a burger with :\n" + 
-                    "${meat.text}" +
-                    (if (cheese)" \n Cheese" else "") +
-                    (if (onions) " \n Onions" else "") +
-                    (if (salad) " \n salad" else "")
-            val tvOrder = findViewById<TextView>(R.id.tvOrder)
-            tvOrder.text = orderString
+        btnToat.setOnClickListener {
+            Toast.makeText(this, "I am a toast", LENGTH_SHORT).show()
         }
+
+
+
     }
 
 }
